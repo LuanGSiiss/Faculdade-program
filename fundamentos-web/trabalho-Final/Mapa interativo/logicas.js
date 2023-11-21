aa106ef997ed9ce8feddba66638cf8cab121d3b3
Luan Gerber Siiss <luangerber666@gmail.com>
2023-11-21 17:47:41 -0300

    map-interativo-nv1

---
+++ fundamentos-web/trabalho-Final/Mapa interativo/logicas.js
@@ -0,0 +1,57 @@
+//definiremos sua visualização para as coordenadas geográficas escolhidas e um nível de zoom:
+var map = L.map('map').setView([51.505, -0.09], 13);
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Faz o Circulo
+var circle = L.circle([51.5, -0.11], {
+    color: "red",
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+//Faz o poligono
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+//Adicionar um marcador
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+//Adiciona os Pop-ups
+marker.bindPopup("<b>Fala galera!</b><br>I am Luan Gameplay.").openPopup();
+circle.bindPopup("I am a bolinha.");
+polygon.bindPopup("I am a objeto em formado dúvidoso.");
+
+//Adiciona Pop-ups em camada
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a Popup Maluco!")
+    .openOn(map);
+
+
+//Adiciona um evento
+/* 
+function onMapClick(e) {
+    alert("Você clicou no mapa" + e.latlng);
+}
+
+map.on('click', onMapClick); 
+*/
+
+//Evento com pop-up
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Você clicou no mapa" + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
